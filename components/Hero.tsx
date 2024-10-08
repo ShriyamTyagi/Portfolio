11ab2fde9398deafdc02578a4ff3cef92d9cf03a
shriyam_tyagi <shriyam.tyagi@moonshinetechnology.com>
2024-10-09 00:18:08 +0530

    frantend to full stack

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Shriyam, a Frontend Web Developer
+            Hi! I&apos;m Shriyam, a Full Stack Web Developer
           </p>
 
           <a href="#experience">
